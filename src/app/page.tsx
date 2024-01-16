3e8fc21ad50054de7e2999f6ce4bda29dab54e3b
zhaoth <tianhao.zhao@capgemini.com>
2024-01-16 09:36:55 +0800

    feat: 设置重定向地址 和 修改线上 api 地址

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const router = useRouter();
-  console.log(router);
+  // 当路由变化时进行判断并重定向
+  if (router.pathname === '/') {
+    router.push('/login');
+  }
+
   return (
-    <>
-      <div>root</div>
-    </>
+    <></>
   );
 }
