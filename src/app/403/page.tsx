b4cb211d4b4ebb5fcf859efed385d29676e3fd0d
zhaoth <tianhao.zhao@capgemini.com>
2024-01-25 11:16:37 +0800

    feat: 设置异常处理了页

--- src/app/403/page.tsx
+++ src/app/403/page.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 import { staticRouter } from '@/static/staticRouter';
 import { Result } from 'antd';
 
-export default function NotFound() {
+export default function NoAuthorized() {
   return (
     <Result
       className='flex place-content-center h-screen'
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
+      status="403"
+      title="403"
+      subTitle="Sorry, you are not authorized to access this page."
       extra={ <Link href={`${staticRouter.root}`}>Return Home</Link>}
     />
   );
--- src/app/500/page.tsx
+++ src/app/500/page.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 import { staticRouter } from '@/static/staticRouter';
 import { Result } from 'antd';
 
-export default function NotFound() {
+export default function WentWrong() {
   return (
     <Result
       className='flex place-content-center h-screen'
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
+      status="500"
+      title="500"
+      subTitle="Sorry, something went wrong."
       extra={ <Link href={`${staticRouter.root}`}>Return Home</Link>}
     />
   );
