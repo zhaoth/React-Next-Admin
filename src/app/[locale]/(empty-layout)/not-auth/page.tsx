67d3d6b68261726f20ed193502784fb3588a4626
zhaoth <tianhao.zhao@capgemini.com>
2024-01-25 17:06:30 +0800

    fix: 异常页面名字为数字时build时的错误

--- src/app/[locale]/(empty-layout)/not-auth/page.tsx
+++ src/app/[locale]/(empty-layout)/not-auth/page.tsx
@@ -1,11 +1,11 @@
+'use client';
 import Link from 'next/link';
 import { staticRouter } from '@/static/staticRouter';
 import { Result } from 'antd';
 
-export default function NoAuthorized() {
+export default function NotAuthorized() {
   return (
     <Result
-      className='flex place-content-center h-screen'
       status="403"
       title="403"
       subTitle="Sorry, you are not authorized to access this page."
--- src/app/[locale]/(empty-layout)/not-server/page.tsx
+++ src/app/[locale]/(empty-layout)/not-server/page.tsx
@@ -2,10 +2,9 @@ import Link from 'next/link';
 import { staticRouter } from '@/static/staticRouter';
 import { Result } from 'antd';
 
-export default function WentWrong() {
+export default function NotServer() {
   return (
     <Result
-      className='flex place-content-center h-screen'
       status="500"
       title="500"
       subTitle="Sorry, something went wrong."
