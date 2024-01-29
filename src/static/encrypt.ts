6e2328b8b4330a7d49042574cf60b0b7e9f24f17
zhaoth <tianhao.zhao@capgemini.com>
2024-01-29 15:48:07 +0800

    feat: 添加基于crypto-js的加密解密工具类

---
+++ src/static/encrypt.ts
@@ -0,0 +1,16 @@
+// 使用对称加密方式
+import CryptoJS from 'crypto-js';
+// 替换为你的密钥
+const secretKey = 'your-secret-key';
+
+export const encryptData = (data: Record<string, any>) => {
+  const dataString = JSON.stringify(data);
+  const encrypted = CryptoJS.AES.encrypt(dataString, secretKey);
+  return encrypted.toString();
+}
+
+export const decryptData = (encryptedData: string) => {
+  const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
+  const decryptedDataString = bytes.toString(CryptoJS.enc.Utf8);
+  return JSON.parse(decryptedDataString);
+}
